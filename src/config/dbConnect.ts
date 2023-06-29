8dd20f614b531d48ee6617704fd1f3066fa66844
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-06-29 23:33:36 +0300

    final clean up

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -17,40 +17,14 @@ export async function initialize () {
 async function ensureDbExists (dbName: string): Promise<void> {
   const query = `IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${dbName}') CREATE DATABASE [${dbName}];`;
   const connection = new Connection(dbConfig)
-  const request = new Request(query, err => {
-    if (err) throw new HttpException(500, err.message)
-  })
   return new Promise((resolve, reject) => {
     connection.connect(err => {
       if (err) reject(new HttpException(500, err.message))
+      const request = new Request(query, err => {
+        if (err) throw new HttpException(500, err.message)
+        resolve()
+      })
       connection.execSql(request)
     })
-    resolve()
   })
-
 }
-
-// async function ensureDbExists (dbName: string): Promise<void> {
-//   return new Promise((resolve, reject) => {
-//     const connection = new Connection(dbConfig);
-//     connection.connect((err) => {
-//       if (err) {
-//         console.error(err);
-//         reject(`Connection Failed: ${err.message}`);
-//       }
-
-//       const createDbQuery = `IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${dbName}') CREATE DATABASE [${dbName}];`;
-//       const request = new Request(createDbQuery, (err) => {
-//         if (err) {
-//           console.error(err);
-//           reject(`Create DB Query Failed: ${err.message}`);
-//         }
-
-//         // query executed successfully
-//         resolve()
-//       });
-
-//       connection.execSql(request);
-//     });
-//   });
-// }
