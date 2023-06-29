652deaaaf90f312c42ed91e43372f4d448c4bbfe
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-06-29 21:51:38 +0300

    reduced data for initial script to original size

--- src/config/initialData.ts
+++ src/config/initialData.ts
@@ -10,55 +10,7 @@ const initialData = [
         "color": "black&white",
         "tail_length": 7,
         "weight": 14
-    },
-    {
-        "name": "Vova",
-        "color": "red",
-        "tail_length": 6,
-        "weight": 22
-    },
-    {
-        "name": "Tania",
-        "color": "yellow",
-        "tail_length": 2,
-        "weight": 40
-    },
-    {
-        "name": "Vadim",
-        "color": "black",
-        "tail_length": 90,
-        "weight": 14
-    },
-    {
-        "name": "Andriy",
-        "color": "white",
-        "tail_length": 7,
-        "weight": 2
-    },
-    {
-        "name": "Lol",
-        "color": "orange",
-        "tail_length": 100,
-        "weight": 1
-    },
-    {
-        "name": "Kostia",
-        "color": "blue",
-        "tail_length": 3,
-        "weight": 3
-    },
-    {
-        "name": "Moxie",
-        "color": "green",
-        "tail_length": 144,
-        "weight": 5
-    },
-    {
-        "name": "Groovy",
-        "color": "black&white",
-        "tail_length": 400,
-        "weight": 14
-    },
+    }
 ]
 
 export default initialData
