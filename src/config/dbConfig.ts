7bbe74942d2e955b0b244022f9bde2bbc8454b42
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-06-29 21:10:50 +0300

    refactored db connection

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,16 +1,23 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const dbName = process.env.DB_NAME
+const userName = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+
 const dbOptions = {
-  "dbName": "dogs",
-  "dbConfig": {
-    "server": "mssql",
-    "options": {
-      "port": 1433,
-      "trustServerCertificate": true
+  dbName: dbName as string,
+  dbConfig: {
+    server: "mssql",
+    options: {
+      port: 1433,
+      trustServerCertificate: true
     },
-    "authentication": {
-      "type": "default",
-      "options": {
-        "userName": "sa",
-        "password": "StrongPass#123"
+    authentication: {
+      type: "default",
+      options: {
+        userName: userName as string,
+        password: password as string
       }
     }
   }
--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -3,36 +3,25 @@ import dbOptions from './dbConfig'
 import addInitialData from './initialScript'
 import sequelize from './Sequelize'
 import NewDog from '../models/Dog'
+import HttpException from '../exeptions/HttpException'
 
 const { dbName, dbConfig } = dbOptions
 
 export async function initialize () {
-  await ensureDbExists(dbName);
+  ensureDbExists(dbName)
   await NewDog.sync({ alter: true })
-  await sequelize.sync({ alter: true });
+  await sequelize.sync({ alter: true })
   await addInitialData()
 }
 
-async function ensureDbExists (dbName: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const connection = new Connection(dbConfig)
-    connection.connect((err) => {
-      if (err) {
-        console.error(err);
-        reject(`Connection Failed: ${err.message}`);
-      }
-
-      const createDbQuery = `IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${dbName}') CREATE DATABASE [${dbName}];`;
-      const request = new Request(createDbQuery, (err) => {
-        if (err) {
-          console.error(err);
-          reject(console.log(`Create DB Query Failed: ${err.message}`));
-        }
-
-        resolve();
-      });
-
-      connection.execSql(request);
-    });
-  });
+function ensureDbExists (dbName: string): void {
+  const query = `IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${dbName}') CREATE DATABASE [${dbName}];`;
+  const connection = new Connection(dbConfig)
+  const request = new Request(query, err => {
+    if (err) throw new HttpException(500, err.message)
+  })
+  connection.connect(err => {
+    if (err) throw new HttpException(500, err.message)
+    connection.execSql(request)
+  })
 }
